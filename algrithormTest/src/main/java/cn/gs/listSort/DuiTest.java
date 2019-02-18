407381011be495fc18012a0a8bbf70df069f4c2f
gaoshuai <15l>
2019-02-18 11:45:43 +0800

    堆排序

---
+++ algrithormTest/src/main/java/cn/gs/listSort/DuiTest.java
@@ -0,0 +1,77 @@
+package cn.gs.listSort;
+
+import java.util.Arrays;
+
+/**
+ * @Description:堆排序
+ * @Author: gaoshuai
+ * @Date: 2019/2/18 10:29
+ */
+//https://www.cnblogs.com/jingmoxukong/p/4303826.html
+/*
+（1）根据初始数组去构造初始堆（构建一个完全二叉树，保证所有的父结点都比它的孩子结点数值大）。
+（2）每次交换第一个和最后一个元素，输出最后一个元素（最大值），然后把剩下元素重新调整为大根堆。
+ */
+public class DuiTest {
+
+    public static void heapAdjust(int[] array, int parent, int length) {
+        int temp = array[parent]; // temp保存当前父节点
+        int child = 2 * parent + 1; // 先获得左孩子
+
+        while (child < length) {
+            // 如果父结点的值已经大于孩子结点的值，则直接结束
+            if (temp >= array[child])
+                break;
+
+            // 如果有右孩子结点，并且右孩子结点的值大于左孩子结点，则选取右孩子结点
+            if (child + 1 < length && array[child] < array[child + 1]) {
+                child++;
+            }
+
+
+            // 把孩子结点的值赋给父结点
+            array[parent] = array[child];
+
+            // 选取孩子结点的左孩子结点,继续向下筛选
+            parent = child;
+            child = 2 * child + 1;
+        }
+
+        array[parent] = temp;
+    }
+
+    public static void heapSort(int[] list) {
+        // 循环建立初始堆
+        for (int i = list.length / 2; i >= 0; i--) {
+            heapAdjust(list, i, list.length);
+        }
+
+        // 进行n-1次循环，完成排序
+        for (int i = list.length - 1; i > 0; i--) {
+            // 最后一个元素和第一元素进行交换
+            int temp = list[i];
+            list[i] = list[0];
+            list[0] = temp;
+
+            // 筛选 R[0] 结点，得到i-1个结点的堆
+            heapAdjust(list, 0, i);
+            System.out.format("第 %d 趟: \t", list.length - i);
+            System.out.println(Arrays.toString(list));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        // 初始化一个序列
+        int[] array = {
+                1, 3, 4, 5, 2, 6, 9, 7, 8, 0
+        };
+
+        // 调用快速排序方法
+        System.out.print("排序前:\t");
+        System.out.println(Arrays.toString(array));
+        heapSort(array);
+        System.out.print("排序后:\t");
+        System.out.println(Arrays.toString(array));
+    }
+}
